a4ad521b4abe2e577277c743e3017724e0d91cc0
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2021-10-01 10:22:13 +0300

    app refactoring, used express.Router

---
+++ data/index.js
@@ -0,0 +1,16 @@
+const BOOKS = [
+    {
+        title: "Iliad",
+        description: 'The Iliad is an epic poem written by the Greek poet Homer. It tells the story of the last year of the Trojan War fought between the city of Troy and the Greeks.',
+        authors: "Homer",
+        fileCover: "soft",
+    },
+    {
+        title: "The Clouds",
+        description: 'The Clouds is a Greek comedy play written by the playwright Aristophanes. A lampooning of intellectual fashions in classical Athens, it was originally produced at the City Dionysia in 423 BC and was not as well received as the author had hoped, coming last of the three plays competing at the festival that year. It was revised between 420 and 417 BC and was thereafter circulated in manuscript form.',
+        authors: "Aristophanes",
+        fileCover: "soft",
+    }
+];
+
+module.exports.BOOKS = BOOKS;
---
+++ routes/books/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+
+const {BOOKS} = require('../../data');
+const {Book} = require('../../models');
+
+const store = {
+    books: BOOKS.map(book => new Book(book.title, book.description, book.authors, book.fileCover)),
+};
+
+router.get('/', (req, res) => {
+    res.send(store.books);
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const book = store.books.find(item => item.id === id);
+
+    if (!book) {
+        res.status(404);
+        res.send('404 | not found');
+        return;
+    }
+
+    res.send(book);
+});
+
+router.post('/', (req, res) => {
+    const { title, description, authors, fileCover } = req.body;
+
+    if (!title || !description || !authors || !fileCover) {
+        res.status(400);
+        res.send('Bad request');
+        return;
+    }
+
+    const newBook = new Book(title, description, authors, fileCover);
+    store.books.push(newBook);
+    res.status(201);
+    res.send(newBook);
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, description, authors, fileCover } = req.body;
+
+    const updatedBookId = store.books.findIndex(item => item.id === id);
+
+    if (updatedBookId === -1) {
+        res.status(404);
+        res.send('404 | not found');
+        return;
+    }
+
+    const bookForUpdate = store.books[updatedBookId];
+
+    const updatedBook = {
+        ...bookForUpdate,
+        title: title || bookForUpdate.title,
+        description: description || bookForUpdate.description,
+        authors: authors || bookForUpdate.authors,
+        fileCover: fileCover || bookForUpdate.fileCover,
+    };
+
+    store.books.splice(updatedBookId, 1, updatedBook);
+    res.send(updatedBook);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const deletedBookId = store.books.findIndex(item => item.id === id);
+
+    if (deletedBookId === -1) {
+        res.status(404);
+        res.send('404 | not found');
+        return;
+    }
+
+    store.books.splice(deletedBookId, 1);
+    res.send(true);
+});
+
+module.exports = router;
