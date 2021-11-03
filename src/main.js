60bc650e7372647a9f21b02546ac08a93ffdfd02
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2021-11-04 00:01:49 +0300

    implemented authentication using passport.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const session = require('express-session');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+
 const userRouter = require('./routes/user');
 const booksRouter = require('./routes/books');
 
@@ -9,28 +15,68 @@ const PasswordDB = process.env.DB_PASSWORD || 'password123';
 const NameDB = process.env.DB_NAME || 'book_library';
 const HostDB = process.env.DB_HOST || 'mongo://localhost:27017';
 
-const app = express();
+mongoose.connect(HostDB, {
+    user: UserDB,
+    pass: PasswordDB,
+    dbName: NameDB,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-async function start() {
-    try {
-        await mongoose.connect(HostDB, {
-            user: UserDB,
-            pass: PasswordDB,
-            dbName: NameDB,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+function verify(username, password, done) {
+    User.findOne({username: username}, function (err, user) {
+        console.log('user', user);
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false);
+        }
 
-        app.listen(PORT);
-    } catch (e) {
-        console.log(e)
-    }
+        if (user.password !== password) {
+            return done(null, false);
+        }
+
+        // `user` будет сохранен в `req.user`
+        return done(null, user);
+    });
 }
+
+const options = {
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: false,
+}
+
+const app = express();
 
 app.use(express.json());
 
-app.use('/api/user', userRouter);
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Конфигурирование Passport для сохранения пользователя в сессии
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    User.findById(id, function (err, user) {
+        if (err) { return cb(err) }
+        cb(null, user);
+    });
+});
+
+//  Добавление стратегии для использования
+passport.use('local', new LocalStrategy(options, verify));
+
+app.use('/api/user', userRouter);
 app.use('/api/books', booksRouter);
 
-start();
+app.listen(PORT);
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,10 +1,80 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-    const user = { id: 1, mail: "test@mail.ru" };
-    res.status(201);
-    res.send(user);
+const User = require('../../models/user');
+
+router.get('/login', (req, res) => {
+    res.send('login');
 });
+
+router.post('/login',
+    passport.authenticate(
+        'local',
+        {
+            successRedirect: '/',
+            failureRedirect: '/api/user/login',
+        },
+    ),
+    (req, res) => {
+        console.log('req.user: ', req.user);
+        res.redirect('/')
+    });
+
+router.post('/signup',
+    async (req, res) => {
+        if (req.isAuthenticated()) {
+            if (req.session) {
+                return res.redirect('/');
+            }
+        }
+
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            res.status(400);
+            res.send('Bad request');
+            return;
+        }
+
+        const user = await User.findOne({username: username});
+
+        if (user) {
+            res.status(409);
+            res.send('A user with this username exists!');
+            return;
+        }
+
+        const newUser = new User({username, password});
+
+        try {
+            await newUser.save();
+
+            res.status(201);
+            res.send(`You are registered successfully! Your username: ${username}`);
+        } catch (e) {
+            console.log(e);
+            res.status(500);
+        }
+    });
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/profile', (req, res, next) => {
+        console.log('req.isAuthenticated()', req.isAuthenticated());
+        if (!req.isAuthenticated || !req.isAuthenticated()) {
+            if (req.session) {
+                req.session.returnTo = req.originalUrl || req.url;
+            }
+            return res.redirect('/api/user/login');
+        }
+        next();
+    },
+    (req, res) => {
+        res.send(`user: ${req.user}`);
+    });
 
 module.exports = router;
