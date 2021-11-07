b37ab4fa8b4d6ff3ec8fafe68a5a51042376425d
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2021-11-07 20:00:42 +0300

    added saving reviews in mongodb

--- src/webSockets/bookReviews/index.js
+++ src/webSockets/bookReviews/index.js
@@ -1,10 +1,19 @@
+const Review = require('../../models/review');
+
 const onBookReviewsConnection = async (io, socket) => {
     const {id} = socket;
     const {roomName} = socket.handshake.query;
     console.log('socket connected:', id, 'roomName', roomName);
 
+    let reviews = [];
+
     if (roomName) {
         socket.join(roomName);
+        reviews = await Review.find({bookId: roomName}).select('-_id author text');
+    }
+
+    if (reviews && reviews.length > 0) {
+        socket.emit('review-list-to-book', reviews);
     }
 
     socket.on('disconnect', () => {
@@ -14,6 +23,18 @@ const onBookReviewsConnection = async (io, socket) => {
     socket.on('review-to-book', (review) => {
         socket.to(roomName).emit('review-to-book', review);
         socket.emit('review-to-book', review);
+
+        const newReview = new Review({
+            bookId: roomName,
+            author: review.author,
+            text: review.text,
+        });
+
+        try {
+            newReview.save();
+        } catch (e) {
+            console.log(e);
+        }
     });
 };
 
