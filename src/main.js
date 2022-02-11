c3057d74c75adf8a1828711f32139884a061d2ca
Aleksandr Svistunov <aleksandr.svistunov@quadcode.com>
2022-02-11 15:20:59 +0400

    init .ts files, set tsconfig

--- src/main.js
+++ src/main.js
@@ -1,100 +1,71 @@
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const User = require('./models/user');
-const onBookReviewsConnection = require('./webSockets/bookReviews');
-
-const booksRouter = require('./routes/books');
-const booksApiRouter = require('./routes/api/books');
-const userApiRouter = require('./routes/api/user');
-
-const PORT = process.env.PORT || 3000;
-const UserDB = process.env.DB_USERNAME || 'root';
-const PasswordDB = process.env.DB_PASSWORD || 'password123';
-const NameDB = process.env.DB_NAME || 'book_library';
-const HostDB = process.env.DB_HOST || 'mongo://localhost:27017';
-
-mongoose.connect(HostDB, {
-    user: UserDB,
-    pass: PasswordDB,
-    dbName: NameDB,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-function verify(username, password, done) {
-    User.findOne({username: username}, function (err, user) {
-        console.log('user', user);
-        if (err) {
-            return done(err);
-        }
-        if (!user) {
-            return done(null, false);
-        }
-
-        if (user.password !== password) {
-            return done(null, false);
-        }
-
-        // `user` будет сохранен в `req.user`
-        return done(null, user);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
+};
+const Book = require('../../models/book');
+class BooksRepository {
+    static getBooks() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return Book.find().select('-__v');
+            }
+            catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+    }
+    static getBook(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return Book.findById(id);
+            }
+            catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+    }
+    static createBook({ title, description, authors, fileCover, fileName }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const newBook = new Book({ title, description, authors, fileCover, fileName });
+                return newBook.save();
+            }
+            catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+    }
+    static deleteBook(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return Book.findByIdAndDelete(id);
+            }
+            catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+    }
+    static updateBook(id, params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                return Book.findByIdAndUpdate(id, Object.assign({}, params));
+            }
+            catch (e) {
+                console.log(e);
+                return null;
+            }
+        });
+    }
 }
-
-const options = {
-    usernameField: 'username',
-    passwordField: 'password',
-    passReqToCallback: false,
-}
-
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.use(express.json());
-
-app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Конфигурирование Passport для сохранения пользователя в сессии
-passport.serializeUser(function (user, cb) {
-    cb(null, user.id);
-});
-
-passport.deserializeUser(function (id, cb) {
-    User.findById(id, function (err, user) {
-        if (err) { return cb(err) }
-        cb(null, user);
-    });
-});
-
-//  Добавление стратегии для использования
-passport.use('local', new LocalStrategy(options, verify));
-
-app.get('/', (req, res) => res.redirect('/books'));
-app.use('/books', booksRouter);
-app.use('/api/books', booksApiRouter);
-app.use('/api/user', userApiRouter);
-
-io.of('/reviews').on('connection', (socket) => {
-    onBookReviewsConnection(io, socket);
-});
-
-server.listen(PORT);
+module.exports = BooksRepository;
+//# sourceMappingURL=main.js.map
